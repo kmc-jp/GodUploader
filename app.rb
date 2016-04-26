905996c6ecbb3f56aa183f15906cb15a40bc625c
Kata <KataOcean@gmail.com>
2016-04-27 01:49:21 +0900

    とりあえずそれっぽい遷移が行われるようにしたぞ！

--- app.rb
+++ app.rb
@@ -74,41 +74,10 @@ helpers do
 
 end
 
-get '/js/time.js' do
+get '/js/upload.js' do
   content_type :js
-  erb :"time.js", :layout => nil
+  erb :"upload.js", :layout => nil
 end
-
-get '/getsession' do
-  se = getSessionByid( params[:id] )
-  content_type :json
-  if se == nil 
-    { isstart:false , isfinished:false }.to_json
-  else
-    { isstart: se.isstart , isfinished: se.isfinished }.to_json
-  end
-end
-
-post '/endsession' do
-  se = getSessionByid( params[:id] )
-  se.isfinished = true  
-  se.save
-end
-
-get '/autoreload' do
-
-  se = getSessionByid( params[:id] )
-
-  result = false
-  if se.autoreload != session[:autoreload] then
-    result = true
-  end
-
-  session[:autoreload] = se.autoreload
-  { autoreload:result  }.to_json
-
-end
-
 
 post '/confirmpassword/:id' do
   se = getSessionByid(params[:id])
@@ -123,193 +92,60 @@ post '/tweetwithimage/:id' do
  redirect main(se)
 end
 
-#強制終了
-post '/forceEnd/:id' do
-   
-  se = getSessionByid(params[:id])
- 
-  se.isstart = false
-  se.autoreload = se.autoreload + 1
-  session[:autoreload] = se.autoreload
-  se.save
-  
-  redirect main(se)
+get '/upload'do
 
-end
+  erb :upload
 
-#終了
-post '/end/:id' do
-  se = getSessionByid( params[:id] )
-  redirect main(se)
-end
-
-#開始時の処理
-post '/start/:id' do
-  
-  se = getSessionByid( params[:id] )
-
-  se.endtime = Time.now + (params[:hours].to_i*(60*60)) + (params[:minutes].to_i*60) + (params[:seconds].to_i)
-#   se.endtime = Time.now + (60*60)
-  se.autoreload = se.autoreload + 1
-  session[:autoreload] = se.autoreload
-
-  se.isstart = true
-  
-  se.save
- 
-  redirect main(se)
-
-end
-
-#お題のアップロード
-post '/uploadOdai/:id' do
-  
-  se = getSessionByid( params[:id] )
-
-  se.autoreload = se.autoreload + 1
-  session[:autoreload] = se.autoreload
-
-  se.odai = ""
-
-  if params[:odaiFile]
-    
-    buf = []
-    params[:odaiFile][:tempfile].each_line do |line|     
-      buf.push(line.force_encoding("UTF-8"))
-    end
-    
-    p buf    
-    se.odai = buf.join( ',' )
-  end
-
-  se.selectedodai = se.odai.split(',').sample(3).join(',')
-
-  se.save
-
-  redirect main( se )
-end
-
-#お題の獲得
-post '/getOdai/:id' do
-  
-  se = getSessionByid( params[:id] )
-
-  se.selectedodai = se.odai.split(',').sample(3).join(',')
-
-  se.autoreload = se.autoreload + 1
-  session[:autoreload] = se.autoreload
-
-  se.save
-
-  redirect main(se)
-
-end
-
-#画像のアップロード
-post '/uploadImage/:id' do
-  
-  se = getSessionByid(params[:id])
-
-  if params[:image]
-    
-    save_path = se.imagepath + "/#{Time.now.strftime("%y%m%d%H%M%S")}_#{rand(99999)}_#{params[:image][:filename]}"
-  
-    if !File.exists?( save_path )
-      File.open( save_path , 'wb' ) do |f|
-        f.write(params[:image][:tempfile].read)
-      end
-    end
-  end  
-
-  redirect main(se)
-
-end
-
-post '/open' do
-
-    se = Session.new;
-    se.name = params[:name]
-    se.password = params[:password]
-    
-    se.selectedodai = ""
-    se.odai = ""
-    se.autoreload = 0
-    session[:autoreload] = se.autoreload
-
-    se.save
-
-    session[:password] = params[:password]
-
-    if File.exists?( uri( 'public/sessions', false ) )
-      Dir.mkdir( uri( 'public/sessions' , false ) )
-    end
-    Dir.mkdir( se.path )
-    Dir.mkdir( se.imagepath )
-
-    redirect main(se)
 end
 
 get '/:id' do
   
 
-  @session = getSessionByid(params[:id])
-  if @session == nil 
+#  @session = getSessionByid(params[:id])
+#  if @session == nil 
       
-    redirect "/error"
+#    redirect "/error"
 
-  else 
-
-    session[:autoreload] = @session.autoreload
-
-    #お題を表示するための配列作り
-    @odai = []
-
-    if @session.selectedodai != nil    
-      @session.selectedodai.split( ',' ).each do |line|
-        @odai.push( line )
-      end
-    end 
+#  else 
 
     #アップロードされた画像を表示するための配列づくり
     @images = []
-    Dir.glob( @session.imagepath + "/*" ).each do |image|
-      @images.push( image.gsub( @session.imagepath , "" ) )
-    end
+#    Dir.glob( @session.imagepath + "/*" ).each do |image|
+#      @images.push( image.gsub( @session.imagepath , "" ) )
+#    end
 
-    @ismanager = false
-    if @session.password == session[:password]
-      @ismanager = true
-    end
     erb :main
 
-  end  
+#  end  
+   
+end 
+
+get '/mypage' do
+  
+
+#  @session = getSessionByid(params[:id])
+#  if @session == nil 
+      
+#    redirect "/error"
+
+#  else 
+
+    #アップロードされた画像を表示するための配列づくり
+    @images = []
+#    Dir.glob( @session.imagepath + "/*" ).each do |image|
+#      @images.push( image.gsub( @session.imagepath , "" ) )
+#    end
+
+    erb :main
+
+#  end  
    
 end 
 
 #インデックス
 get '/' do
 
-  session[:password] = ""  
+  session[:id] = ""  
 
-  @waitsessions = [] #開始前のセッション
-  @drawingsessions = [] #ワンドロ中のセッション
-  @endsessions = [] #終了済みのセッション
-
-  Session.all.each do |s|
-    if s.isstart
-      if s.isfinished
-        @endsessions.push(s)
-      else 
-        @drawingsessions.push(s)
-      end 
-    else
-      @waitsessions.push(s)
-    end
-  end
-
-  @waitsessions = @waitsessions.sort {|a,b| b.created_at <=> a.created_at }
-  @drawingsessions = @drawingsessions.sort {|a,b| b.created_at <=> a.created_at}
-  @endsessions = @endsessions.sort {|a,b| b.created_at <=> a.created_at}
-  
   erb :index
 end
