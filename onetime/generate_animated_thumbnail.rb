fe592c73cfb2a6fe5f31d9f8b17362673102d539
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-11-10 00:28:05 +0900

    onetime scriptも追従

--- onetime/generate_animated_thumbnail.rb
+++ onetime/generate_animated_thumbnail.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/ruby
 # サムネイル画像を生成する
 # 高さ186pxに合わせる
-require 'tmpdir'
-
 outdir = './public/thumbnail'
 
 Dir.glob("./public/illusts/*.gif") {|image|
@@ -11,10 +9,5 @@ Dir.glob("./public/illusts/*.gif") {|image|
     outfile = "#{outdir}/#{basename}#{ext}"
     puts outfile
 
-    Dir.mktmpdir {|tmpdir|
-        mid_gif = "#{tmpdir}/mid.gif"
-
-        system "convert -coalesce #{image} #{mid_gif}"
-        system "convert #{mid_gif} -resize x186 #{outfile}"
-    }
+    system "convert #{image} -coalesce -resize x186 -layers optimize #{outfile}"
 }
